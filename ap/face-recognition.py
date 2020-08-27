9c8abddfefc7e977a2b4c376bfd0df4aef6ab5d9
Huy Ho <tuanhuy0496@yahoo.com>
2020-08-27 15:27:37 +0700

    Face Reconigtion Script

--- ap/face-recognition.py
+++ ap/face-recognition.py
@@ -5,25 +5,19 @@ Acknowledgement
 This code is adapted from:
 https://www.pyimagesearch.com/2018/06/18/face-recognition-with-opencv-python-and-deep-learning/
 """
-
+import cv2
+import face_recognition
+from imutils.video import VideoStream
+import imutils
 import pickle
 import time
 
-import cv2
-import face_recognition
-import imutils
-# import the necessary packages
-from imutils.video import VideoStream
 
 
-class FaceId:
-    """
-    Provide the static recognise method
-    identified by username and matched face
-    """
+class FaceId():
 
     @staticmethod
-    def recognise(username):
+    def main(userName):
         """
         Compare the username with the recorded user face database
         :param username: username that need to compare with the saved images
@@ -36,22 +30,21 @@ class FaceId:
 
         # initialize the video stream and then allow the camera sensor to warm up
         print("[INFO] Starting Video Stream...")
-        video_stream = VideoStream(src=0).start()
+        videoStream = VideoStream(src=0).start()
         time.sleep(2.0)
 
-        attempt_count = 0
-        while attempt_count <= 10:
+        attemptCount = 0
+        while attemptCount <= 10:
             # grab the frame from the threaded video stream
-            frame = video_stream.read()
+            frame = videoStream.read()
 
-            # convert the input frame from BGR to RGB then resize it to have
-            # a width of 750px (to speedup processing)
+            # convert frame from BGR to RGB
+            # resize a width of size 750px (this is for speeding purpose)
             rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             rgb = imutils.resize(frame, width=240)
 
             # detect the (x, y)-coordinates of the bounding boxes
-            # corresponding to each face in the input frame, then compute
-            # the facial embeddings for each face
+            # corresponding to each face in the input frame
             boxes = face_recognition.face_locations(rgb, model="hog")
             encodings = face_recognition.face_encodings(rgb, boxes)
             names = []
@@ -61,19 +54,17 @@ class FaceId:
                 # attempt to match each face in the input image to our known
                 # encodings
                 matches = face_recognition.compare_faces(data["encodings"], encoding)
-                name = "Unknown"
+                name = "Unidentified"
 
                 # check to see if we have found a match
                 if True in matches:
                     # find the indexes of all matched faces then initialize a
-                    # dictionary to count the total number of times each face
-                    # was matched
-                    matched_index = [i for (i, b) in enumerate(matches) if b]
+                    # dictionary to count the total number of times each face was matched
+                    matchedIndex = [i for (i, b) in enumerate(matches) if b]
                     counts = {}
 
-                    # loop over the matched indexes and maintain a count for
-                    # each recognized face face
-                    for i in matched_index:
+                    # loop over the matched indexes and maintain a count for each recognized face face
+                    for i in matchedIndex:
                         name = data["names"][i]
                         counts[name] = counts.get(name, 0) + 1
 
@@ -86,14 +77,13 @@ class FaceId:
                 names.append(name)
 
             # loop over the recognized faces
-            if username in names:
+            if userName in names:
                 # release the camera resource
-                video_stream.stop()
+                videoStream.stop()
                 return True
             time.sleep(1.0)
-            attempt_count += 1
+            attemptCount += 1
 
-        # do a bit of cleanup
-        video_stream.stop()
-        print("Can't find the matched face data")
+        videoStream.stop()
+        print("Error: Can't find the matched face")
         return False
