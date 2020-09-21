d82a03d6d23db3e358567d056233a895962d6942
deirpes <deirpes@gmail.com>
2020-09-21 10:43:09 +0700

    Add middleware for home

--- rest-api/app/web/pages/index.js
+++ rest-api/app/web/pages/index.js
@@ -1,5 +1,20 @@
-const Home = () => (
-    <h1>Home</h1>
-);
+import { useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import AppContext from '../context/AppContext';
+
+const Home = () => {
+    const { authenticated } = useContext(AppContext);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (authenticated) {
+            router.push('/user');
+        } else {
+            router.push('/auth/login');
+        }
+    }, []);
+
+    return <></>;
+};
 
 export default Home;
