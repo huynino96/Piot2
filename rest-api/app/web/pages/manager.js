0d0a127e8e13a7bc4b7adfac8cc483756d4f2434
deirpes <deirpes@gmail.com>
2020-09-22 10:59:49 +0700

    Fixed data visualization

--- rest-api/app/web/pages/manager.js
+++ rest-api/app/web/pages/manager.js
@@ -1,23 +1,6 @@
-import { useState, useEffect } from 'react';
-import { Card, CardHeader, CardBody } from 'reactstrap';
-import api from '../api';
+import { CardHeader, CardBody } from 'reactstrap';
 
 const Manager = () => {
-    const [data, setData] = useState([]);
-    const [iserror, setIserror] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    useEffect(() => {
-        api.get("/users")
-            .then(res => {
-                setData(res.data.data)
-            })
-            .catch(error=>{
-                setErrorMessage(["Cannot load user data"])
-                setIserror(true)
-            });
-    }, []);
-
     return (
         <CardBody>
             <CardHeader>
